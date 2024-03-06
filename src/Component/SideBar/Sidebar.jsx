5c49c15a84a22c939afc5f948c8dc29143e6ddde
ananathmukesh <ananthmukesh625@gmail.com>
2024-03-06 15:14:24 +0530

    UPDATE SOME data

--- src/Component/SideBar/Sidebar.jsx
+++ src/Component/SideBar/Sidebar.jsx
@@ -9,114 +9,108 @@ import { MdBusinessCenter } from "react-icons/md";
 import { MdElectricRickshaw } from "react-icons/md";
 import { FaLandmark } from "react-icons/fa";
 import { BiCategory } from "react-icons/bi";
-import { MdInfoOutline } from "react-icons/md";
-import { FiPhoneCall } from "react-icons/fi";
-import { IoMdLogIn } from "react-icons/io";
-import { useMediaQuery } from '@mui/material';
-
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "./Side.css";
 
-const Sidebar = ({setSidebarOpen}) => {
+const Sidebar = () => {
+  const navigate = useNavigate();
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-
-  const isSmallScreen = useMediaQuery('(max-width:768px)');
-
 
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
   };
 
+
+  const handlelogout = () => {
+    localStorage.removeItem('auth1');
+    navigate('/');
+  }
+
   return (
     <div className="Sidebar p-4">
-      <div
-        className="side d-flex  flex-column justify-content-between"
-        style={{ position: "relative", height: "100%" }}
-      >
-        <div className="">
-          <div className="topside d-flex align-items-center justify-content-between">
-            <img src={logo} className="logo" alt="logo" />
-            <div className="ts-icons">
-              <IoHeartOutline className="ts-icon" />
-              <GoBell className="ts-icon" />
-              {/* <Link to="/main/jobs"> */}
+      <div className="side">
+        <div className="topside d-flex align-items-center justify-content-between">
+          <img src={logo} className="logo" alt="logo" />
+          <div className="ts-icons">
+            <IoHeartOutline className="ts-icon" />
+            <GoBell className="ts-icon" />
+            <Link to="/main/jobs ">
               <FaRegUserCircle className="ts-icon text-white" />
-              {/* </Link> */}
-              <Link to="/">
-                <IoPowerOutline className="ts-icon text-white" />
-              </Link>
-            </div>
+            </Link>
+            <IoPowerOutline className="ts-icon text-white" onClick={handlelogout}/>
           </div>
-          {/* <div className="ts-middle mt-4">
-            <ul className="p-0 mb-0">
-              <li
-                className="d-flex justify-content-between align-items-center category"
-                style={{
-                  padding: "5px 8px 5px 8px",
-                  borderRadius: "8px",
-                }}
-                onClick={toggleDropdown}
-              >
-                <div className="d-flex align-items-center">
-                  <BiCategory className="icon" />
-                  <p className="mb-0 ms-4 cate-text">Categories</p>
-                </div>
-                {isDropdownOpen ? (
-                  <TbCaretDownFilled className="open-caret" />
-                ) : (
-                  <TbCaretUpFilled className="open-caret" />
-                )}
-              </li>
-              {isDropdownOpen && (
-                <div className="d-flex align-items-center">
-                  <ul className="p-0 w-100" style={{ listStyleType: "none" }}>
+        </div>
+        <div className="ts-middle mt-4">
+          <ul className="p-0">
+            <li
+              className="d-flex justify-content-between align-items-center category"
+              style={{
+                padding: "5px 8px 5px 8px",
+                borderRadius: "8px",
+              }}
+              onClick={toggleDropdown}
+            >
+              <div className="d-flex align-items-center">
+                <BiCategory className="icon" />
+                <p className="mb-0 ms-4 cate-text">CATEGORIES</p>
+              </div>
+              {isDropdownOpen ? (
+                <TbCaretDownFilled className="open-caret" />
+              ) : (
+                <TbCaretUpFilled className="open-caret" />
+              )}
+            </li>
+            {/* {isDropdownOpen && (
+              <div className="d-flex align-items-center">
+                <ul className="p-0 w-100" style={{ listStyleType: "none" }}>
+                  <li
+                    className="mb-0 category-type text-white"
+                    style={{
+                      listStyleType: "none",
+                      padding: "9px 0px 9px 57px",
+                      borderRadius: "8px",
+                    }}
+                  >
+                    <MdBusinessCenter className="me-3 fs-5" />
+                    Jobs
+                  </li>
+
+                  <li
+                    className="mb-0 category-type"
+                    style={{
+                      listStyleType: "none",
+                      padding: "9px 0px 9px 57px",
+                      borderRadius: "8px",
+                    }}
+                  >
+                    <FaLandmark className="me-3 fs-5" /> Construction
+                  </li>
+                  <Link to="/main/vehicle">
                     <li
                       className="mb-0 category-type text-white"
                       style={{
                         listStyleType: "none",
                         padding: "9px 0px 9px 57px",
                         borderRadius: "8px",
+                        textDecoration: "none",
                       }}
                     >
-                      <MdBusinessCenter className="me-3 fs-5" />
-                      Jobs
+                      <MdElectricRickshaw className="me-3 fs-5" />
+                      Vehicles
                     </li>
-
-                    <li
-                      className="mb-0 category-type"
-                      style={{
-                        listStyleType: "none",
-                        padding: "9px 0px 9px 57px",
-                        borderRadius: "8px",
-                      }}
-                    >
-                      <FaLandmark className="me-3 fs-5" /> Property
-                    </li>
-                    <Link to="/main/vehicle">
-                      <li
-                        className="mb-0 category-type text-white"
-                        style={{
-                          listStyleType: "none",
-                          padding: "9px 0px 9px 57px",
-                          borderRadius: "8px",
-                          textDecoration: "none",
-                        }}
-                      >
-                        <MdElectricRickshaw className="me-3 fs-5" />
-                        Vehicles
-                      </li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </ul>
-          </div> */}
+                  </Link>
+                </ul>
+              </div>
+            )} */}
+          </ul>
         </div>
-        {/* <div
+        <div
           className="ts-bottom"
           style={{ position: "absolute", bottom: "25" }}
         >
+          {/* <ThemeToggleButton /> */}
           <div class="form-check form-switch">
             <input
               class="form-check-input"
@@ -124,85 +118,6 @@ const Sidebar = ({setSidebarOpen}) => {
               id="flexSwitchCheckChecked"
             />
           </div>
-        </div> */}
-        <div
-          className="terms"
-          // style={{ position: "absolute", top: "31rem" }}
-        >
-          <ul className="p-0 mb-0">
-            <li
-              className="d-flex justify-content-between align-items-center category"
-              style={{
-                padding: "5px 8px 5px 8px",
-                borderRadius: "8px",
-              }}
-            >
-              <Link
-                to="/main/About"
-                style={{ color: "#fff", textDecoration: "none" }}
-              >
-                <div className="d-flex mt-2 align-items-center">
-                  <i class="fi fi-rr-building"></i>
-                  <p  onClick={ isSmallScreen ? () => setSidebarOpen(false) :  undefined} className="mb-0 ms-4 cate-text text-wrap"> About Us </p>
-                </div>
-              </Link>
-            </li>
-            <li
-              className="d-flex justify-content-between align-items-center category"
-              style={{
-                padding: "5px 8px 5px 8px",
-                borderRadius: "8px",
-              }}
-            >
-              <Link
-                to="/main/Terms"
-                style={{ color: "#fff", textDecoration: "none" }}
-              >
-                <div className="d-flex mt-2 align-items-center">
-                  <MdInfoOutline className="icon" />
-                  <p   onClick={ isSmallScreen ? () => setSidebarOpen(false) :  undefined}
-                 
-                   className="mb-0 ms-4 cate-text text-wrap">
-                    Terms & Condition
-                  </p>
-                </div>
-              </Link>
-            </li>
-            <li
-              className="d-flex mt-2 justify-content-between align-items-center category"
-              style={{
-                padding: "5px 8px 5px 8px",
-                borderRadius: "8px",
-              }}
-            >
-              <Link
-                to="/main/ContactUs"
-                style={{ color: "#fff", textDecoration: "none" }}
-              >
-                <div className="d-flex align-items-center">
-                  <FiPhoneCall className="icon" />
-                  <p  onClick={ isSmallScreen ? () => setSidebarOpen(false) :  undefined} className="mb-0 ms-4 cate-text">Contact Us</p>
-                </div>
-              </Link>
-            </li>
-            <li
-              className="d-flex mt-2 justify-content-between align-items-center category"
-              style={{
-                padding: "5px 8px 5px 8px",
-                borderRadius: "8px",
-              }}
-            >
-              {/* <Link
-                to="https://getbootstrap.com/docs/5.3/forms/form-control/"
-                style={{ color: "#fff", textDecoration: "none" }}
-              >
-                <div className="d-flex align-items-center">
-                  <IoMdLogIn className="icon" />
-                  <p className="mb-0 ms-4 cate-text">Employee Login</p>
-                </div>
-              </Link> */}
-            </li>
-          </ul>
         </div>
       </div>
     </div>
